d2cec0b0da871aff132ef6fae192556d9084b555
amandakicke <amandakicke@gmail.com>
2022-04-09 08:32:42 -0300

    primeiro commit

---
+++ JavaApplication7/src/javaapplication7/NewClass.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication7;
+
+/**
+ *
+ * @author Amanda2
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World, I'm learning Java!");
+    }
+}
+//psvm menans our main method
+//sout means I want to print something
+//main method is the first thing that is running
---
+++ JavaApplication7/src/javaapplication7/Test.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication7;
+
+/**
+ *
+ * @author Amanda2
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Java is so important language for programmer");
+    }
+}
