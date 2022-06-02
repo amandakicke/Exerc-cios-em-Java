7f99dff9da8df96ab02c96a9a347f1f844018b64
amandakicke <amandakicke@gmail.com>
2022-06-02 08:08:12 -0300

    commit

---
+++ curso_programacao/src/Funcoes.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Funcoes {
+
+	public static void main(String[] args) {
+		/* Fazer um programa para ler três números
+		 inteiros e mostrar na tela o maior deles.*/
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter three numbers:");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		/*if (a > b && a > c) {
+			System.out.println("Higher = " + a);
+		} else if (b > c) {
+			System.out.println("Higher = " + b);
+		} else {
+			System.out.println("Higher = " + c);
+		}*/
+		
+		int higher = max(a, b, c);
+		
+		showResult(higher);
+		
+		
+		sc.close();
+	}
+	
+	public static int max(int x, int y, int z) {
+		int aux;
+		if (x > y && x > z) {
+			aux = x;
+		}
+		else if (y > z) {
+			aux = y;
+		} 
+		else {
+			aux = z;
+		}
+		return aux;
+	}
+	
+	public static void showResult(int value) {
+		System.out.println("Higher = " + value);
+	}
+
+}
