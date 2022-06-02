64ca62be41048943387184d2c6cc19df39096ed0
amandakicke <amandakicke@gmail.com>
2022-06-02 11:06:53 -0300

    commit

--- course/src/application/Program.java
+++ course/src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Triangle;
+
 public class Program {
 
 	public static void main(String[] args) {
@@ -15,24 +17,46 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		Locale.setDefault(Locale.US);
 
-		double xA, xB, xC, yA, yB, yC;
+		/*
+		 * double xA, xB, xC, yA, yB, yC;
+		 * 
+		 * System.out.println("Enter the measures of triangle X: "); xA =
+		 * sc.nextDouble(); xB = sc.nextDouble(); xC = sc.nextDouble();
+		 * 
+		 * System.out.println("Enter the measures of triangle Y: "); yA =
+		 * sc.nextDouble(); yB = sc.nextDouble(); yC = sc.nextDouble();
+		 * 
+		 * double p = (xA + xB + xC) / 2.0; double areaX = Math.sqrt(p * (p - xA) * (p -
+		 * xB) * (p - xC));
+		 * 
+		 * p = (yA + yB + yC) / 2.0; double areaY = Math.sqrt(p * (p - yA) * (p - yB) *
+		 * (p - yC));
+		 * 
+		 * System.out.printf("Triangle X area: %.4f%n", areaX);
+		 * System.out.printf("Triangle Y area: %.4f%n", areaY);
+		 * 
+		 * if (areaX > areaY) { System.out.println("Larger area: X"); } else {
+		 * System.out.println("Larger area: Y"); }
+		 */
+
+		Triangle x, y;
+		x = new Triangle();
+		y = new Triangle();
 
 		System.out.println("Enter the measures of triangle X: ");
-		xA = sc.nextDouble();
-		xB = sc.nextDouble();
-		xC = sc.nextDouble();
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
 
 		System.out.println("Enter the measures of triangle Y: ");
-		yA = sc.nextDouble();
-		yB = sc.nextDouble();
-		yC = sc.nextDouble();
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
 
-		double p = (xA + xB + xC) / 2.0;
-		double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+		double areaX = x.area();
+		double areaY = y.area();
 
-		p = (yA + yB + yC) / 2.0;
-		double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
-
+		
 		System.out.printf("Triangle X area: %.4f%n", areaX);
 		System.out.printf("Triangle Y area: %.4f%n", areaY);
 
